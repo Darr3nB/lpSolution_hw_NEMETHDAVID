9ce0fd34e238c96455a78fb0ea6235473f309211
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2023-04-03 10:14:46 +0200

    HTML basic code generated based on command line arguments

--- lpSolution_hw_NEMETHDAVID/src/main/java/org/example/Main.java
+++ lpSolution_hw_NEMETHDAVID/src/main/java/org/example/Main.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringBuilder htmlBuilder = new StringBuilder();
+        htmlBuilder.append("<!DOCTYPE html><html><head><title>Németh Dávid - Teszt Feladat</title></head><body>");
+        htmlBuilder.append("<h1>Teszt Feladat</h1><p><a href=\"https://github.com/Darr3nB/lpSolution_hw_NEMETHDAVID\">Megoldás</a></p>");
+        htmlBuilder.append("<p>Németh Dávid tel.: +36309937525 e-mail: david.nemeth024@gmail.com LinkedIn: https://www.linkedin.com/in/d%C3%A1vid-n%C3%A9meth-08401024b/</p>");
+        htmlBuilder.append("<table border=\"1 px solid black\">");
+
+        for (int i = 0; i < args.length; i += 2) {
+            String name = args[i];
+            String mail = args[i + 1];
+
+            htmlBuilder.append(String.format("<tr><td>Név</td><td>%s</td></tr><tr><td>Elérhetőség</td><td>%s</td></tr>", name, mail));
+        }
+
+        htmlBuilder.append("</table><a href=\"http://lpsolutions.hu\">L&P Solutions</a></body></html>");
+        String html = htmlBuilder.toString();
+
+        System.out.println(html);
     }
 }
