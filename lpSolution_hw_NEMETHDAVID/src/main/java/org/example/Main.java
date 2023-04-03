ca750952f6e4a1c7b19555591f1fc5c05ec34ec5
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2023-04-03 10:40:19 +0200

    Generated HTML code now saved in file called output.html

--- lpSolution_hw_NEMETHDAVID/src/main/java/org/example/Main.java
+++ lpSolution_hw_NEMETHDAVID/src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +10,7 @@ public class Main {
         StringBuilder htmlBuilder = new StringBuilder();
         htmlBuilder.append("<!DOCTYPE html><html><head><title>Németh Dávid - Teszt Feladat</title></head><body>");
         htmlBuilder.append("<h1>Teszt Feladat</h1><p><a href=\"https://github.com/Darr3nB/lpSolution_hw_NEMETHDAVID\">Megoldás</a></p>");
-        htmlBuilder.append("<p>Németh Dávid tel.: +36309937525 e-mail: david.nemeth024@gmail.com LinkedIn: https://www.linkedin.com/in/d%C3%A1vid-n%C3%A9meth-08401024b/</p>");
+        htmlBuilder.append("<p>Németh Dávid Tel.: +36309937525 E-mail: david.nemeth024@gmail.com LinkedIn: https://www.linkedin.com/in/d%C3%A1vid-n%C3%A9meth-08401024b/</p>");
         htmlBuilder.append("<table border=\"1 px solid black\">");
 
         for (int i = 0; i < args.length; i += 2) {
@@ -30,5 +33,15 @@ public class Main {
         tagRemoveScanner.close();
 
         System.out.println(html);
+
+        try {
+            File outputFile = new File("output.html");
+            FileWriter fileWriter = new FileWriter(outputFile);
+            fileWriter.write(html);
+            fileWriter.close();
+            System.out.println("HTML code saved to output.html");
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving the HTML code to file: " + e.getMessage());
+        }
     }
 }
